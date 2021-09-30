c5cbdae270971a2175773b6bbd2d0494896778d4
Bhavy airi <airiraghav@gmail.com>
2021-09-30 17:38:08 +0530

    the index file and to note that i created it using xampp

---
+++ index.php
@@ -0,0 +1,6 @@
+<h3> Login area </h3>
+<form action ="check.php" method="post">
+<input type ="text" name ="usr" ><br>
+<input type ="password" name ="pwd"><br>
+<input type ="submit" value="Login">
+</form>
